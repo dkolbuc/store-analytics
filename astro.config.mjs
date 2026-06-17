bbdc05c406df035955837ccd2e89b8db57d9adae
dkolbuc <d.kolbuc21@gmail.com>
2026-06-17 06:03:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ export default defineConfig({
   output: "server",
   adapter: cloudflare({
     // Włącza lokalny proxy dla bindings (D1, KV, itp.) podczas `astro dev`
+  security: {
+    checkOrigin: false,
+    },
     platformProxy: {
       enabled: true,
       // Wskazuje na lokalne bindingi zdefiniowane w wrangler.toml
